bca38810995a40e5cc4e48b571bca3dea7052022
Jianliang <j.gao@imperial.ac.uk>
2023-07-19 09:58:39 +0100

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,5 +1,4 @@
-#install.packages("quantmod")
-install.packages("quantmod_0.4-14.tar.gz", repos = NULL, type = "source")
+install.packages("quantmod")
 install.packages("dplyr")
 install.packages("plotly")
 install.packages("lubridate")
